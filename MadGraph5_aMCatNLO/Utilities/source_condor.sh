fe5afeb2147eaa2c41d3c562140c6c3388df3f52
ldellape <luca.dellapenna@pg.infn.it>
2025-01-21 09:46:28 +0100

    first commit

---
+++ MadGraph5_aMCatNLO/Utilities/source_condor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# HTCondor python bindings are lost after cmsenv/scram
+#PYTHON_BINDINGS="$(python3 -c 'import htcondor; import os; print(os.path.dirname(htcondor.__path__[0]))' 2>/dev/null)"
+#if [ -z "$PYTHON_BINDINGS" ]; then
+#export _condor_SCHEDD_HOST=bigbird13.cern.ch
+#export _condor_CREDD_HOST=bigbird13.cern.ch 
+
+if [ -d $CMSSW_BASE/venv ];
+then 
+  echo "venv exists"
+else
+  scram-venv
+  cmsenv
+  pip3 install htcondor --upgrade #FIXME need better way to interface HTCondor Python API for Python3.9
+fi
